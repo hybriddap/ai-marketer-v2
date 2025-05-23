c33dcc2521b4cd6959674798e4abf6dd64c8d062
Heeran Kim <heerankim3@gmail.com>
2025-05-24 00:25:08 +1000

    Fixed square auth 401 error and added redis

--- ai-marketer-v2-backend/backend/businesses/urls.py
+++ ai-marketer-v2-backend/backend/businesses/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import BusinessDetailView, SquareViewSet
+from .views import BusinessDetailView, SquareViewSet, square_oauth_callback
 
 router = DefaultRouter()
 router.include_root_view = False
@@ -10,6 +10,8 @@ router.register(r'square', SquareViewSet, basename='square')
 
 urlpatterns = [
     path("me/", BusinessDetailView.as_view(), name="business-detail"),
+
+    path('square/callback/', square_oauth_callback, name='square-auth-callback'),
 
     # Square endpoints
     path('', include(router.urls)),
